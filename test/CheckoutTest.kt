ee3e2dcb9b2252aa0ef657ae44f4f113ca3a6501
Christoffer G. Thomsen <chris@cgt.name>
2019-08-02 18:34:13 +0200

    Sell 0 items

--- test/CheckoutTest.kt
+++ test/CheckoutTest.kt
@@ -1,7 +1,16 @@
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class CheckoutTest {
     @Test
-    fun nothing() {
+    fun `sell zero items`() {
+        val checkout = Checkout()
+        assertEquals(0, checkout.total())
+    }
+}
+
+class Checkout {
+    fun total(): Int {
+        return 0
     }
 }
