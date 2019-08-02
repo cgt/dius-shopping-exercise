1fd5efce9371f45d14def4d1bb5787f2546e220e
Christoffer G. Thomsen <chris@cgt.name>
2019-08-02 18:39:03 +0200

    Sell one item

--- test/CheckoutTest.kt
+++ test/CheckoutTest.kt
@@ -7,10 +7,26 @@ class CheckoutTest {
         val checkout = Checkout()
         assertEquals(0, checkout.total())
     }
+
+    @Test
+    fun `sell one item`() {
+        val checkout = Checkout()
+        checkout.scan("ipd")
+        assertEquals(54999, checkout.total())
+    }
 }
 
 class Checkout {
+    private var lastScanned: String? = null
+
     fun total(): Int {
+        if (lastScanned != null) {
+            return 54999
+        }
         return 0
+    }
+
+    fun scan(sku: String) {
+        lastScanned = sku
     }
 }
