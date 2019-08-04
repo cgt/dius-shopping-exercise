f085d523a4eca21db8b68ff279d832f07bcb440b
Christoffer G. Thomsen <chris@cgt.name>
2019-08-04 13:06:16 +0200

    Extract free VGA with MBP discount to function

--- src/main/kotlin/name/cgt/dius/shopping/Checkout.kt
+++ src/main/kotlin/name/cgt/dius/shopping/Checkout.kt
@@ -13,14 +13,19 @@ class Checkout(
                 priceInCentsBySku.getOrDefault(item, 0)
             }
             .sum()
-        val mbpQuantity = scanned.count { it == "mbp" }
-        val vgaQuantity = scanned.count { it == "vga" }
-        val freeVGAs = min(mbpQuantity, vgaQuantity)
-        total -= freeVGAs * priceInCentsBySku.getValue("vga")
+        total -= freeVGAWithPurchaseOfMBPDiscount(scanned, priceInCentsBySku)
         return total
     }
 
     fun scan(sku: String) {
         scanned.add(sku)
     }
+}
+
+private fun freeVGAWithPurchaseOfMBPDiscount(items: ArrayList<String>, catalog: Map<String, Int>): Int {
+    val mbpQuantity = items.count { it == "mbp" }
+    val vgaQuantity = items.count { it == "vga" }
+    val freeVGAs = min(mbpQuantity, vgaQuantity)
+    val discount = freeVGAs * catalog.getValue("vga")
+    return discount
 }
